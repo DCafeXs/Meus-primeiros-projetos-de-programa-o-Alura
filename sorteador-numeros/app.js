00a72370c59779fa674574d94ae0378e00542154
Diogo Cafe <dgpassoscafe123@gmail.com>
2026-08-17 12:35:02 -0300

    Puxando valores e testando.

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,10 @@
+function sortear() {
+    let quantidadeDeNumeros = document.getElementById("quantidade").value;
+    let doNumero = document.getElementById("de").value;
+    let ateNumero = document.getElementById("ate").value;
+
+    
+    alert(`quantidade de numeros ${quantidadeDeNumeros}`)
+    alert(`do numero ${doNumero}`)
+    alert(`ate o numero ${ateNumero}`)
+}
