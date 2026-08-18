7cd486b314a24b4a95d5a99ecc806b539451b2ef
Diogo Cafe <dgpassoscafe123@gmail.com>
2026-08-18 12:49:50 -0300

    feat: melhora a função de sortear números para suportar múltiplos valores

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -2,11 +2,16 @@ function sortear() {
     let quantidadeDeNumeros = parseInt(document.getElementById("quantidade").value);
     let doNumero = parseInt(document.getElementById("de").value);
     let ateNumero = parseInt(document.getElementById("ate").value);
+    
+    let sorteados =[];
 
-    let numero = obterNumero(doNumero , ateNumero);
-    exibirResultado(numero);
+    for(let i = 0; i < quantidadeDeNumeros; i++) {
+        let numero = obterNumero(doNumero , ateNumero);
+        sorteados.push(numero);
+        
+    }
 
-
+    exibirResultado(sorteados);
 }
 function obterNumero(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
