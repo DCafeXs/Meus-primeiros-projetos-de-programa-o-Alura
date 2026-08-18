722b8e09d4d77b234d2795031bc62cce3eebb5da
Diogo Cafe <dgpassoscafe123@gmail.com>
2026-08-18 12:35:14 -0300

    feat: adiciona função para exibir número sorteado e corrige função obterNumero

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -3,10 +3,17 @@ function sortear() {
     let doNumero = parseInt(document.getElementById("de").value);
     let ateNumero = parseInt(document.getElementById("ate").value);
 
-    let numero = obterNumero(doNumero , ateNumero)
-    alert(`numero sorteado ${numero}`)
+    let numero = obterNumero(doNumero , ateNumero);
+    exibirResultado(numero);
+
 
 }
 function obterNumero(min, max) {
-    return Math.floor(Math.random() * (max - min) + min);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+function exibirResultado(numeroSorteado){
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML =  `<label class="texto__paragrafo">Números sorteados:os numeros sorteados foram ${numeroSorteado}</label>`
+    
+}  
