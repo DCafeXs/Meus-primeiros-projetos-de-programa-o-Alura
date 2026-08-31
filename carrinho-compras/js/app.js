14663de11b96a923290046339c33881237136911
Diogo Cafe <dgpassoscafe123@gmail.com>
2026-08-31 12:57:27 -0300

    feat(carrinho): processa dados do produto e insere no array

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,14 +1,20 @@
 let carrinho=[];
 
 function adicionar(){
-    let produto = document.getElementById("produto");
+    let produto = document.getElementById("produto").value;
     let quantidade = document.getElementById("quantidade").value;
 
     if (!quantidade || quantidade <= 0){
         alert("Digite uma quantidade valida.");
         return;
     }
-    carrinho.push(produto);
-    console.log(carrinho);
+    let nomeProduto = produtoSelect.split(" - ")[0];
+    let valorUnitario = parseFloat(produtoSelect.split("R$")[1]);
+    let precoTotal = quantidade * valorUnitario;
 
+    carrinho.push({
+      nome: nomeProduto,
+      quantidade: quantidade,
+      preco: precoTotal
+});
 }
