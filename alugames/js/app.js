600a2d6802c164668006eee3d1268bfe3b753f1a
Diogo Cafe <dgpassoscafe123@gmail.com>
2026-08-31 12:24:41 -0300

    feat:implementa captura inicial de dados do formulário

--- alugames/js/app.js
+++ alugames/js/app.js
@@ -2,14 +2,22 @@ function alterarStatus(id) {
     let jogo = document.getElementById(`game-${id}`);
     let botao = jogo.querySelector(".dashboard__item__button");
     let img = jogo.querySelector(".dashboard__item__img");
-    let name = jogo.querySelector(".dashboard__item__name");
 
-    if (botao.classList.contains("dashboard__item__button--return")){
-        botao.textContent= "Alugar";
-    }else {
-        botao.textContent= "Devolver";
+    if (botao.classList.contains("dashboard__item__button--return")) {
+        let check = prompt("Você quer mesmo devolver o jogo? (Digite 'sim')");
+        
+        if (check && check.toLowerCase() === "sim") {
+            botao.textContent = "Alugar";
+            img.classList.toggle("dashboard__item__img--rented");
+            botao.classList.toggle("dashboard__item__button--return");
+        }
+    } else {
+        let check2 = prompt("Você quer mesmo alugar o jogo? (Digite 'sim')");
+    
+        if (check2 && check2.toLowerCase() === "sim") {
+            botao.textContent = "Devolver";
+            img.classList.toggle("dashboard__item__img--rented");
+            botao.classList.toggle("dashboard__item__button--return");
+        }
     }
-
-    img.classList.toggle("dashboard__item__img--rented");
-    botao.classList.toggle("dashboard__item__button--return");
 }
