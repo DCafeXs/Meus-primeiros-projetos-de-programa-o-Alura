29f35f5a859a83e220cd62ab49c13209dcafc629
Diogo Cafe <dgpassoscafe123@gmail.com>
2026-08-31 12:59:56 -0300

    feat(carrinho): renderiza itens do array na interface e atualiza total

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -8,13 +8,30 @@ function adicionar(){
         alert("Digite uma quantidade valida.");
         return;
     }
-    let nomeProduto = produtoSelect.split(" - ")[0];
-    let valorUnitario = parseFloat(produtoSelect.split("R$")[1]);
+    
+    // Ajustado de produtoSelect para produto
+    let nomeProduto = produto.split(" - ")[0];
+    let valorUnitario = parseFloat(produto.split("R$")[1]);
     let precoTotal = quantidade * valorUnitario;
 
     carrinho.push({
       nome: nomeProduto,
       quantidade: quantidade,
       preco: precoTotal
-});
+    });
+
+    let listaCarrinho = document.getElementById("lista-produtos");
+    listaCarrinho.innerHTML = ""; 
+    let valorTotalGeral = 0;
+
+    carrinho.forEach(item => {
+        valorTotalGeral += item.preco;
+        listaCarrinho.innerHTML += `
+            <section class="carrinho__produtos__produto">
+                <span class="texto-azul">${item.quantidade}x</span> ${item.nome} <span class="texto-azul">R$${item.preco}</span>
+            </section>
+        `;
+    });
+
+    document.getElementById("valor-total").textContent = `R$ ${valorTotalGeral}`;
 }
